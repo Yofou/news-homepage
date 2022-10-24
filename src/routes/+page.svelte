5e7fe467523a8b89fa5ea9eb21d43ffa1ce1a2f9
Yofou <exodias1965@gmail.com>
2022-10-24 07:07:07 +0100

    chore: landmark + title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,10 @@
 	}
 </script>
 
-<h1 class="sr-only">Web 3.0</h1>
+<header class="sr-only">
+	<h1>Web 3.0</h1>
+</header>
+
 <main class="w-screen bg-white-300 min-h-screen grid grid-cols-[minmax(0,1100px)] justify-center">
 	<div class="w-full grid grid-rows-[max-content,max-content] pt-[28px] pb-[75px] md:pt-[90px] md:pb-[130px] px-4">
 		<nav class="grid gap-[40px] grid-cols-[max-content,1fr] md:grid-cols-[max-content,1fr,repeat(3,max-content)] mb-[55px]">
